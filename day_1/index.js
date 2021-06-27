f81d7d887a8eac68230ee05923fd2643240e60e9
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2021-06-27 12:53:05 +0200

    finishing project 1.

--- day_1/index.js
+++ day_1/index.js
@@ -1,13 +1,10 @@
 const cards = document.querySelectorAll('.card')
 
-// On click event for each section
 cards.forEach((card)=>{
 card.addEventListener('click',()=>{
-    // remove active class from all another section
-    // and add it to the selected section
     cards.forEach((card) => {
-        card.classList.remove('active')
+        card.setAttribute('class', 'card')
     })
-    card.classList.add('active')
+    card.setAttribute('class', 'card active')
 })
 })
